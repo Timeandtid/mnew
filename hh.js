a28f75eb8d9c5d25ea1b49d304fdb2c99ff400a5
AAron <82711833+Timeandtid@users.noreply.github.com>
2021-06-01 13:30:15 +0300

    Create hh.js

---
+++ hh.js
@@ -0,0 +1,6 @@
+//Get HTML elements
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var file = document.getElementById("3dFile");
+var upload = document.getElementById("3dUpload")
